03bf50afa7080a0c89b222c82688eb7ffe53a6ff
cainthebest <caindavies@live.co.uk>
2022-05-17 02:30:22 +0100

    impl(endpoint): Gameserver - Files - List

--- src/interface/api/game_server.ts
+++ src/interface/api/game_server.ts
@@ -231,4 +231,29 @@ export interface Game_Server {
             { file: string }
         >;
     }>;
+    '/services/{id}/gameservers/file_server/list': URL<{
+        get: RequestResponse<
+            {
+                status: '200';
+                body: {
+                    status: 'success';
+                    data: {
+                        entries: Array<{
+                            type: string;
+                            path: string;
+                            name: string;
+                            size: number;
+                            owner: string;
+                            group: string;
+                            chmod: string;
+                            created_at: number;
+                            modified_at: number;
+                            accessed_at: number;
+                        }>;
+                    };
+                };
+            },
+            { dir?: string; search?: string }
+        >;
+    }>;
 }
