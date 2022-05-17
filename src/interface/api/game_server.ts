0e66eff898f6eb44eaa7ca21b820044f33a82494
cainthebest <caindavies@live.co.uk>
2022-05-17 03:43:49 +0100

    impl(endpoint): Gameserver - Full Game list

--- src/interface/api/game_server.ts
+++ src/interface/api/game_server.ts
@@ -350,4 +350,53 @@ export interface Game_Server {
             { path: string; file: string }
         >;
     }>;
+    '/gameserver/games': URL<{
+        get: RequestResponse<{
+            status: '200';
+            body: {
+                status: 'success';
+                data: {
+                    games: {
+                        steam_id?: string;
+                        locations: Array<{
+                            id: number;
+                            country: string;
+                            city: string;
+                        }>;
+                        games: Array<{
+                            id: string;
+                            steam_id: number;
+                            has_steam_game: string;
+                            name: string;
+                            minecraft_mode: boolean;
+                            publicserver_only: boolean;
+                            portlist_short: string;
+                            folder_short: string;
+                            minimum_slots: number;
+                            slot_multiplier: unknown;
+                            maximum_recommended_slots: number;
+                            modpacks: Array<{
+                                id?: number;
+                                name?: string;
+                                modpack_version?: string;
+                                modpack_file?: string;
+                                visible?: boolean;
+                                game_version?: string;
+                            }>;
+                            icons: {
+                                x256: string;
+                                x120: string;
+                                x64: string;
+                                x32: string;
+                                x16: string;
+                            };
+                            tags: Array<unknown>;
+                            preorder_locations: Array<unknown>;
+                            locations: Array<number>;
+                        }>;
+                    };
+                };
+            };
+        }>;
+    }>;
 }
