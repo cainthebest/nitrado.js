1ad24ef760a256e219ec68110f38cc0079018f5f
cainthebest <cainthebest@users.noreply.github.com>
2022-05-17 02:44:32 +0000

    chore(build)

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -252,6 +252,141 @@ interface Game_Server {
             search?: string;
         }>;
     }>;
+    '/services/{id}/gameservers/file_server/move': URL<{
+        post: RequestResponse<{
+            status: '200';
+            body: {
+                status: 'success';
+                message: 'The directory has been moved';
+            };
+        }, {
+            source_path: string;
+            target_path: string;
+            target_filename?: string;
+        }>;
+    }>;
+    '/services/{id}/gameservers/file_server/seek': URL<{
+        get: RequestResponse<{
+            status: '200';
+            body: {
+                status: 'success';
+                data: {
+                    token: {
+                        url: string;
+                        token: string;
+                    };
+                };
+            };
+        }, {
+            file: string;
+            offset: number;
+            length: number;
+            mode?: 'raw' | 'lines';
+        }>;
+    }>;
+    '/services/{id}/gameservers/file_server/size': URL<{
+        get: RequestResponse<{
+            status: '200';
+            body: {
+                status: 'success';
+                data: {
+                    size: number;
+                };
+            };
+        }, {
+            path: string;
+        }>;
+    }>;
+    '/services/{id}/gameservers/file_server/stat': URL<{
+        get: RequestResponse<{
+            status: '200';
+            body: {
+                status: 'success';
+                data: {
+                    entries: Array<{
+                        type: string;
+                        path: string;
+                        name: string;
+                        size: number;
+                        owner: string;
+                        group: string;
+                        chmod: string;
+                        created_at: number;
+                        modified_at: number;
+                        accessed_at: number;
+                    }>;
+                };
+            };
+        }, {
+            files: Array<string>;
+        }>;
+    }>;
+    '/services/{id}/gameservers/file_server/upload': URL<{
+        post: RequestResponse<{
+            status: '200';
+            body: {
+                status: 'success';
+                data: {
+                    token: {
+                        url: string;
+                        token: string;
+                    };
+                };
+            };
+        }, {
+            path: string;
+            file: string;
+        }>;
+    }>;
+    '/gameserver/games': URL<{
+        get: RequestResponse<{
+            status: '200';
+            body: {
+                status: 'success';
+                data: {
+                    games: {
+                        steam_id?: string;
+                        locations: Array<{
+                            id: number;
+                            country: string;
+                            city: string;
+                        }>;
+                        games: Array<{
+                            id: string;
+                            steam_id: number;
+                            has_steam_game: string;
+                            name: string;
+                            minecraft_mode: boolean;
+                            publicserver_only: boolean;
+                            portlist_short: string;
+                            folder_short: string;
+                            minimum_slots: number;
+                            slot_multiplier: unknown;
+                            maximum_recommended_slots: number;
+                            modpacks: Array<{
+                                id?: number;
+                                name?: string;
+                                modpack_version?: string;
+                                modpack_file?: string;
+                                visible?: boolean;
+                                game_version?: string;
+                            }>;
+                            icons: {
+                                x256: string;
+                                x120: string;
+                                x64: string;
+                                x32: string;
+                                x16: string;
+                            };
+                            tags: Array<unknown>;
+                            preorder_locations: Array<unknown>;
+                            locations: Array<number>;
+                        }>;
+                    };
+                };
+            };
+        }>;
+    }>;
 }
 
 interface Domain {
