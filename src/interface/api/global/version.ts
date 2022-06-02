3608b7e28bdd0ecc44d9b4d146d177b1563d4f92
cainthebest <caindavies@live.co.uk>
2022-06-02 14:17:04 +0100

    doc(version): add comments

--- src/interface/api/global/version.ts
+++ src/interface/api/global/version.ts
@@ -1,5 +1,7 @@
 import { URL, RequestResponse } from '../../../types/url';
 
+// Global - Request current API version
+// Docs: https://doc.nitrado.net/#api-Global-GetVersion
 export interface Version {
     '/version': URL<{
         get: RequestResponse<{
