eed17bf1542370dcbe03ace9a33f1402e3493fd5
cainthebest <cainthebest@users.noreply.github.com>
2022-06-04 23:35:15 +0000

    chore(build)

--- src/types/api/service.ts
+++ src/types/api/service.ts
@@ -1,11 +1,17 @@
-import { Services } from '../../interface/api/service/services'
-import { Cancel } from '../../interface/api/service/cancel'
-import { AutoExtend } from '../../interface/api/service/auto_extend'
-import { SubDomain } from '../../interface/api/service/sub_domain'
-import { KnowledgeBase } from '../../interface/api/service/knowledge_base'
-import { SalePrice } from '../../interface/api/service/sale_price'
-import { Logs } from '../../interface/api/service/logs'
-import { Notifications } from '../../interface/api/service/notifications'
+import { Services } from '../../interface/api/service/services';
+import { Cancel } from '../../interface/api/service/cancel';
+import { AutoExtend } from '../../interface/api/service/auto_extend';
+import { SubDomain } from '../../interface/api/service/sub_domain';
+import { KnowledgeBase } from '../../interface/api/service/knowledge_base';
+import { SalePrice } from '../../interface/api/service/sale_price';
+import { Logs } from '../../interface/api/service/logs';
+import { Notifications } from '../../interface/api/service/notifications';
 
-export type Service = Services & Cancel & AutoExtend & SubDomain & KnowledgeBase & SalePrice & Logs & Notifications;
-
+export type Service = Services &
+    Cancel &
+    AutoExtend &
+    SubDomain &
+    KnowledgeBase &
+    SalePrice &
+    Logs &
+    Notifications;
