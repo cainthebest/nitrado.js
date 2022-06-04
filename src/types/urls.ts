f90763ee8e9d0efe1f82e64407dbff717e53d9ae
cainthebest <caindavies@live.co.uk>
2022-06-05 00:33:52 +0100

    impl(endpoint): service

--- src/types/urls.ts
+++ src/types/urls.ts
@@ -2,5 +2,6 @@ import { Global } from './api/global';
 import { OAuth2 } from './api/oauth2';
 import { Registration } from './api/registration';
 import { User } from './api/user';
+import { Service } from './api/service';
 
-export type URLs = Global & OAuth2 & Registration & User;
+export type URLs = Global & OAuth2 & Registration & User & Service;
